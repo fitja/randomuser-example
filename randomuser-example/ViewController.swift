5d7ec71ce9cbc43b4b86c6e28fef8cd04ac108d2
Filip Pejovic <filip.pejovic@appculture.com>
2019-11-16 15:03:42 +0100

    Initial Commit

---
+++ randomuser-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  randomuser-example
+//
+//  Created by Filip Pejovic on 11/16/19.
+//  Copyright © 2019 Filip Pejovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
