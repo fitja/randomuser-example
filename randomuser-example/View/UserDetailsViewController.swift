ffb6f3ae405aa8946dd5065c60f6aace1ab19753
Filip Pejovic <ipeyovic@gmail.com>
2019-11-17 21:38:58 +0100

    Improve UI for UserDetails VC

--- randomuser-example/View/UserDetailsViewController.swift
+++ randomuser-example/View/UserDetailsViewController.swift
@@ -16,7 +16,7 @@ final class UserDetailsViewController: UIViewController {
     private let imageView = UIImageView()
     private let nameLabel = UILabel()
     private let ageLabel = UILabel()
-    private let emailLabel = UITextView()
+    private let emailButton = UIButton(type: .system)
 
     // MARK: Properties
 
@@ -25,7 +25,7 @@ final class UserDetailsViewController: UIViewController {
             guard let user = user else { return }
             nameLabel.text = user.fullName
             ageLabel.text = "\(user.dob.age)"
-            emailLabel.text = "\(user.email)"
+            emailButton.setTitle(user.email, for: .normal)
             imageView.setImage(from: user.picture.large)
         }
     }
@@ -33,7 +33,7 @@ final class UserDetailsViewController: UIViewController {
     // MARK: Lifecycle
 
     override func loadView() {
-        view = UIView()
+        view = UIScrollView()
     }
 
     override func viewDidLoad() {
@@ -41,55 +41,68 @@ final class UserDetailsViewController: UIViewController {
 
         title = user?.fullName
         view.backgroundColor = .white
-
-        let stack = UIStackView()
-        stack.axis = .vertical
-        stack.spacing = 8
-
-        view.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-        imageView.contentMode = .scaleAspectFit
-
-        view.addSubview(stack)
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
-        stack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
-        stack.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 32).isActive = true
-        stack.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8).isActive = true
-
-        stack.addArrangedSubview(nameLabel)
-        stack.addArrangedSubview(ageLabel)
-        stack.addArrangedSubview(emailLabel)
-        emailLabel.isEditable = false
-        emailLabel.dataDetectorTypes = [.link]
-        emailLabel.delegate = self
+        configureOutlets()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-}
 
-// MARK: - UITextViewDelegate
+    // MARK: Actions
 
-extension UserDetailsViewController: UITextViewDelegate {
-    func textView(_ textView: UITextView,
-                  shouldInteractWith URL: URL,
-                  in characterRange: NSRange,
-                  interaction: UITextItemInteraction) -> Bool {
-
+    @objc
+    private func didTapEmailButton() {
         guard MFMailComposeViewController.canSendMail(), let user = user else {
-            return false
+            return
         }
         let mail = MFMailComposeViewController()
 
         mail.mailComposeDelegate = self
         mail.setToRecipients([user.email])
-        return true
+    }
+
+    // MARK: Helpers
+
+    private func configureOutlets() {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 8
+        view.addSubview(stack)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+        stack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
+        stack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        stack.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+
+        stack.addArrangedSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        let imageHeight = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        imageHeight.priority = .defaultHigh
+        imageHeight.isActive = true
+        imageView.contentMode = .scaleAspectFit
+
+        let nameTitle = UILabel(text: "Name")
+        nameTitle.font = UIFont.boldSystemFont(ofSize: 17)
+        stack.addArrangedSubview(nameTitle)
+
+        nameLabel.font = UIFont.systemFont(ofSize: 15)
+        stack.addArrangedSubview(nameLabel)
+
+        let ageTitle = UILabel(text: "Age")
+        ageTitle.font = UIFont.boldSystemFont(ofSize: 17)
+        stack.addArrangedSubview(ageTitle)
+
+        ageLabel.font = UIFont.systemFont(ofSize: 15)
+        stack.addArrangedSubview(ageLabel)
+
+        let emailTitle = UILabel(text: "Email")
+        emailTitle.font = UIFont.boldSystemFont(ofSize: 17)
+        stack.addArrangedSubview(emailTitle)
+
+        emailButton.addTarget(self, action: #selector(didTapEmailButton), for: .touchUpInside)
+        emailButton.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        emailButton.contentHorizontalAlignment = .left
+        stack.addArrangedSubview(emailButton)
     }
 }
 
@@ -99,5 +112,12 @@ extension UserDetailsViewController: MFMailComposeViewControllerDelegate {
     func mailComposeController(_ controller: MFMailComposeViewController,
                                didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+private extension UILabel {
+    convenience init(text: String) {
+        self.init()
+        self.text = text
     }
 }
